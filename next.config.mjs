e73ad0bc5fe89e668e712c18c2c9f7fce4d00dcb
RAfi <rafi@example.com>
2026-08-11 16:15:29 +0700

    config: configure dynamic basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isProd ? repoName : '',
   images: {
     unoptimized: true,
   },
